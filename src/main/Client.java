c90a5bbff5dd7ede230f30c30b75505e919ddfe5
Basak Cortuk <bcortuk16@ku.edu.tr>
2020-10-29 01:29:35 +0300

    Added connection part

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,8 +1,22 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import connection.Connection;
+
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Connection connection = new Connection();
 	// write your code here
+        String serverHost = "localhost";
+        int serverPort = 9999;
+        connection.startConnection(serverHost,serverPort);
+
     }
+
+
 }
