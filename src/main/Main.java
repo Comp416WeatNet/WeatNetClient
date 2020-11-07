ace5069bce57f9a5c1726a7498766095362b16ed
iremarpag <73463510+iremarpag@users.noreply.github.com>
2020-11-07 05:49:05 +0300

    Imp udpClient  to main

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Client c = new Client(Client.DEFAULT_SERVER_HOST,Client.DEFAULT_SERVER_PORT);
+        udp_client.udpClient();
     }
 
 }
+
+
